7bf46e97ae1af4b75c1d2447e83c5ae5a2f7b193
AlanHee <alan.hee@outlook.com>
2024-12-12 20:12:50 +0800

    Thu

---
+++ github-push-if-changed.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FOLDER_TO_MONITOR=$1
+SCRIPT_TO_EXECUTE=$2
+
+# determine weather inotifywait installed
+if ! type inotifywait &>/dev/null; then
+	apt update -qyy
+	apt upgrade -qyy
+	apt install -qyy inotify-tools
+fi
+
+# inotifywait -m -r -e modify,create,delete,move,attrib "$FOLDER_TO_MONITOR" |
+inotifywait -m -r -e modify,create,delete,move "$FOLDER_TO_MONITOR" |
+	while read directory events filename; do
+		echo "Fold $FOLDER_TO_MONITOR changed: $events $filename"
+		cd $1
+		github-commit-all.sh $(date)
+	done
