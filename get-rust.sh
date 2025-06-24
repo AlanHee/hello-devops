d752efc5af39cfa1297f47f75e0b6f85cc12f483
AlanHee <alan.hee@outlook.com>
2025-06-25 02:37:30 +0800

    Wed

---
+++ get-rust.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# AlanHee (alan.hee@outlook.com) 2025.06.25
+apt install rust rust-analyzer -qy
--- men.sh
+++ men.sh
@@ -1,70 +1,35 @@
 #!/bin/bash
+# Extract command-line options and parameters with getopts
 source funs.sh
+source men-lib.sh
 
-showHelp() {
-	cat <<EOF
-${greenf}
-dt      Dart test
-dte     Dart test -r expande
-dab     Dart pub add dev:build_runner
-daj			Dart pub add dev:json_serializable
-dat			Dart pub add dev:build_test
-drw     Dart run build_runner watch
-drb     Dart run build_runner build
-drs     Dart run build_runner serve
-drt			Dart run build_runner test
-fr      Flutter run
-frr     Flutter run --release
-fr      Flutter clean
-fcr     Flutter clean && flutter run
-u       Flutter unlock thread
-g       Flutter update modules
-fu      Flutter upgrade --force
-fb      Flutter build model to Json
-fba     Flutter build apk
-fbi     Flutter build ios
-e       Flutter open android emulator
-dbi     dart pub global activate --source path .
-dpm     dart pub upgrade --major-versions
-EOF
-}
+# if [ "$1" = "" ]; then
+# 	showHelp
+# 	while read command args; do
+# 		doAction $command
+# 	done
+# elif [ -n $1 ]; then
+# 	doAction $1
+# fi
 
-doAction() {
-	case $1 in
-	dt) dart test ;;
-	dte) dart test -r expanded $2 ;;
-	frr) flutter run --release ;;
-	dab) dart pub add dev:build_runner ;;
-	daj) dart pub add dev:json_serializable ;;
-	dat) dart pub add dev:build_test ;;
-	dbb) dart run build_runner build --delete-conflicting-outputs ;;
-	drw) dart run build_runner watch --delete-conflicting-outputs	;; 
-	drs) dart run build_runner serve	;; 
-	drt) dart run build_runner test	;; 
-	fr) flutter run ;;
-	e | android-emulator) flutter emulators --launch Pixel_3a_API_30_x86 ;;
-	u | flutter-unloack) rm ~/Documents/flutter/bin/cache/lockfile ;;
-	fb) flutter packages pub run build_runner build ;;
-	fba) flutter build apk ;;
-	fbi) flutter build ios ;;
-	fc) flutter clean ;;
-	fcr) flutter clean && flutter run ;;
-	fu) flutter upgrade --force ;;
-	g | flutter) flutter pub get ;;
-	dbi) dart pub global activate --source path . ;;
-	dpm) dart pub upgrade --major-versions ;;
-	quit | q) exit 0 ;;
-	h | help) showHelp ;;
+# men.sh -a -b value1 -cd test1 test2
+while getopts :ab:cdh opt; do
+	case "$opt" in
+	a) echo "Found the -a option" ;;
+	b) echo "Found the -b option with parameter value $OPTARG" ;;
+	c) echo "Found the -c option" ;;
+	d) echo "Found the -d option" ;;
+	h) showHelp ;;
+	*) echo "Unknown option: $opt" ;;
 	esac
-	exit 0
-}
+done
 
-initializeANSI
-if [ "$1" = "" ]; then
-	showHelp
-	while read command args; do
-		doAction $command
-	done
-elif [ -n $1 ]; then
-	doAction $1
-fi
+shift $(($OPTIND - 1))
+
+echo
+count=1
+for param in "$@"; do
+	echo "Parameter $count: $param"
+	count=$(($count + 1))
+done
+exit
